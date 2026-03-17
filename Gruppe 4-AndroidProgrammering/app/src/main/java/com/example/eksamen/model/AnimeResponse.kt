535580ee3bfe1b04444cda56d49c5468e597b323
martinehagberg <martine.hagberg@gmail.com>
2026-03-17 21:17:58 +0100

    Delete Gruppe 4-AndroidProgrammering directory

--- Gruppe 4-AndroidProgrammering/app/src/main/java/com/example/eksamen/model/AnimeResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.eksamen.model
-
-import com.example.eksamen.model.remote.AnimeApiModel
-
-//Representerer den øverste respons-strukturen fra APIer når vi henter en liste med anime.
-data class AnimeResponse (
-    val data: List<AnimeApiModel>
-)
--- Gruppe 4-AndroidProgrammering/app/src/main/java/com/example/eksamen/model/remote/AnimeByIdResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eksamen.model.remote
-
-
-data class AnimeByIdResponse(
-        val data: AnimeApiModel
-    )
